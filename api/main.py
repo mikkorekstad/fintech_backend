cfa1f9d69c4ede46b98e72495722cafda5ae84fb
Uzaaft <muaf@nmbu.no>
2022-01-19 15:37:35 +0100

    Fixed additional api route

--- api/main.py
+++ api/main.py
@@ -1,8 +1,15 @@
 from typing import Dict, List, Optional, Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from api import scraper
+
+
+class DataRequest(BaseModel):
+    ticker_name: str = "^VIX"
+    n_days: int = 30
+
 
 app = FastAPI()
 SCRAPER = scraper.Scraper()
@@ -13,6 +20,6 @@ def get_recommendation():
     return SCRAPER.recommendation(api=1)
 
 
-@app.get("/data")
-def get_data(ticker, n_days=30):
-    return SCRAPER.get_json(ticker_str=ticker, n_days=n_days)
+@app.post("/data/")
+def get_data(data: DataRequest):
+    return SCRAPER.get_json(ticker_str=data.ticker_name, n_days=data.n_days)
