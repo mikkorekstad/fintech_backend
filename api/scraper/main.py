a7d093208bf91c8d56d4d3fe3a64c87aa1abb08b
mikkorekstad <mikkojohan7@gmail.com>
2022-01-19 14:23:01 +0100

    add get_data

--- api/scraper/main.py
+++ api/scraper/main.py
@@ -11,3 +11,8 @@ SCRAPER = scraper.Scraper()
 @app.get("/recommendation")
 def get_recommendation():
     return SCRAPER.recommendation(api=1)
+
+
+@app.get("/data")
+def get_data(ticker, n_days=30):
+    return SCRAPER.get_json(ticker_str=ticker, n_days=n_days)
